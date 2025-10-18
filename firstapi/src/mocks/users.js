a305d4797f0656327979d9568d08e5458d2a3f4a
guilhermepaiva86 <213318639+guilhermepaiva86@users.noreply.github.com>
2025-10-17 23:58:04 -0300

    Commit inicial da pasta local

---
+++ firstapi/src/mocks/users.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Gui',
+    },
+    {
+        id: 2,
+        name: 'Fran',
+    },
+    {
+        id: 3,
+        name: 'Theo',
+    },
+    {
+        id: 4,
+        name: 'Alice',
+    },
+    
+];
