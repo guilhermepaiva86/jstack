990199e5e1a5f2ca7670593d4978e08350cae6eb
guilhermepaiva86 <213318639+guilhermepaiva86@users.noreply.github.com>
2025-10-22 18:17:43 -0300

    ok

---
+++ react-foundations/src/Post.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PostHeader from "./PostHeader";
+
+export default function Post(props) {
+    return (
+            <>
+            <article>
+                <PostHeader 
+                    onRemove={props.onRemove}
+                    post={{
+                        id: props.post.id,
+                        title: props.post.title,
+                        read: props.post.read
+                    }}
+                />
+                <br />
+                <small>{props.post.subtitle}</small>
+            </article>
+            <br />
+            </>
+    );
+}
+
+Post.propTypes = {
+    onRemove: PropTypes.func.isRequired,
+    post: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        read: PropTypes.bool.isRequired,
+    }).isRequired,
+};
