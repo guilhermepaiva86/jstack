1a13538957fdaad500cbb8d67d7be0fdc1140328
guilhermepaiva86 <213318639+guilhermepaiva86@users.noreply.github.com>
2025-10-30 00:01:04 -0300

    119/372

--- mycontacts/fe/src/assets/styles/themes/default.js
+++ mycontacts/fe/src/assets/styles/themes/default.js
@@ -1,3 +1,15 @@
 export default {
-  backgroundColor: '#f6f5fc',
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      main: '#5061FC',
+      light: '#6674f4',
+      lighter: '#E0E3FF',
+      dark: '#3346F0',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+    },
+  },
 };
---
+++ mycontacts/fe/src/pages/Home/index.js
@@ -0,0 +1,88 @@
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar imóvel..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gran Hills</strong>
+              <small>instagram</small>
+            </div>
+            <span>Endereço</span>
+            <span>Descrição</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gran Hills</strong>
+              <small>instagram</small>
+            </div>
+            <span>Endereço</span>
+            <span>Descrição</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gran Hills</strong>
+              <small>instagram</small>
+            </div>
+            <span>Endereço</span>
+            <span>Descrição</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
